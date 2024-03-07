8fb4c47de49aafa3e4b092496bcb4bb5106989ae
Rohan Saluja <saluja@Rohans-MacBook-Pro.local>
2024-03-07 23:50:04 +0530

    removed unused locals restriction

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "@typescript-eslint/no-unused-vars": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
